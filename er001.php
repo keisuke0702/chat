7f0a15e839d5597b018e12b29371c3167e5a43ed
Keisuke Ohtaka <keikun0702@gmail.com>
2016-12-19 15:36:52 +0900

    add chat files

---
+++ er001.php
@@ -0,0 +1,28 @@
+<!--エラー画面　画面ID : ER001-->
+
+<html>
+	<head>
+		<!--Title Name-->
+		<title> Chat - Error001 </title>
+	</head>
+	
+	<body>
+	
+		<form action="wc101.php">
+	
+		<p><h1>Chat</h1></p>
+		
+		<!--ここスタイルシートの使いどころ-->
+		<h2><font color="#FF0000">Error</font></h2>	
+		<h4><font color="#FF0000">Not found.</font></h4>
+		
+		<p><input type="submit" value="back"></p>				
+
+		
+		</form>
+		
+		
+
+	</body>
+	
+</html>
